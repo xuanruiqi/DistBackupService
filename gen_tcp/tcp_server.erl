66c9135c214b164a4ecf35dbebe0f013a61d64d4
bmacnamara3 <bmacnamara3@gmail.com>
2016-11-16 18:23:57 -0500

    started writing client interface

--- gen_tcp/tcp_server.erl
+++ gen_tcp/tcp_server.erl
@@ -4,6 +4,7 @@
 -export([
          start_link/1,
          start_link/0,
+         init_upload/1,
          get_count/0,
          stop/0
          ]).
@@ -18,22 +19,48 @@
 -record(state, {port, lsock, request_count = 0}).
 
 %%%===================================================================
-%%% server init funs
+%%% API
 %%%===================================================================
 
+%%% server init funs
+
 start_link(Port) ->
+	% ?SERVER is an atom
 	gen_server:start_link({local, ?SERVER}, ?MODULE, [Port], []).
 
 start_link() ->
 	start_link(?DEFAULT_PORT).
 
+%%% client funs 
 
-%%%===================================================================
-%%% client funs
-%%%===================================================================
+init_upload(File) ->
 
+	erlang:display("initializing file upload"),
+
+	% hash my fle
+
+	% tell monitor I want to upload My_file
+	{RetMsg, IP_addresses} = gen_server:call(?SERVER, {init_upload, File}),
+
+	erlang:display(RetMsg).
+
+	%upload(File, IP_addresses).
+
+
+% upload(File, IP_addresses) ->
+
+% 	erlang:display("CLIENT: now uploading file to peers"),
+
+% 	% use loop to upload file to each IP_address in IP_addresses
+% 	gen_server:cast(?SERVER, {upload, File}).
+
+
+% only for test. returns request count.
 get_count() ->
 	gen_server:call(?SERVER, get_count).
+
+
+%%% server termination funs
 
 stop() ->
 	gen_server:cast(?SERVER, stop).
@@ -55,6 +82,11 @@ init([Port]) ->
 	% (in handle_info/2) as the first thing you do after initialization
 	{ok, #state{port = Port, lsock = ListenSocket}, 0}.
 
+handle_cast({upload, File}, State) ->
+
+	erlang:display("uploading client file"),
+	{noreply, State};
+
 handle_cast(stop, State) ->
 	{stop, normal, State}.
 
@@ -63,7 +95,6 @@ handle_info({tcp, Socket, RawData}, State) ->
 	do_rpc(Socket, RawData),
 	RequestCount = State#state.request_count,
 	{noreply, State#state{request_count = RequestCount + 1}};
-
 
 % this is the first thing the server does after it has finished
 % running the init/1 function
@@ -85,7 +116,14 @@ handle_call(get_count, _From, State) ->
 	% where N is the current number of requests
 
 	% set new server state to same as before
-	{reply, {ok, State#state.request_count}, State}.
+	{reply, {ok, State#state.request_count}, State};
+
+handle_call({init_upload, File}, _From, State) ->
+
+	Reply = {"recieved init_upload request", [1,2,3,4]},
+
+	% set new server state to same as before
+	{reply, Reply, State}.
 
 terminate(_Reason, _State) ->
 	ok.
