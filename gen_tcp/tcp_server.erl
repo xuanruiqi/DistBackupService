992f75a917abad4672c9a37aba5f1ba08f82f33b
bmacnamara3 <bmacnamara3@gmail.com>
2016-11-18 17:25:04 -0500

    finished skeleton code for client interface

--- gen_tcp/tcp_server.erl
+++ gen_tcp/tcp_server.erl
@@ -4,7 +4,11 @@
 -export([
          start_link/1,
          start_link/0,
+         connect/3,
+         disconnect/1,
          init_upload/1,
+         init_download/1,
+         init_remove/1,
          get_count/0,
          stop/0
          ]).
@@ -33,26 +37,65 @@ start_link() ->
 
 %%% client funs 
 
+connect(IP_address, Port, Options) ->
+
+	{ok, Socket} = gen_tcp:connect(IP_address, Port, Options),
+	Socket.
+
+% throws error
+disconnect(Socket) ->
+
+	gen_tcp:close(Socket).
+
 init_upload(File) ->
 
-	erlang:display("initializing file upload"),
+	erlang:display("CLIENT: initializing file upload"),
 
 	% hash my fle
 
-	% tell monitor I want to upload My_file
+	% tell monitor I want to upload File
 	{RetMsg, IP_addresses} = gen_server:call(?SERVER, {init_upload, File}),
 
-	erlang:display(RetMsg).
+	erlang:display(RetMsg),
 
-	%upload(File, IP_addresses).
+	upload(File, IP_addresses).
 
 
-% upload(File, IP_addresses) ->
+upload(File, IP_addresses) ->
 
-% 	erlang:display("CLIENT: now uploading file to peers"),
+	erlang:display("CLIENT: now uploading file to peers"),
 
-% 	% use loop to upload file to each IP_address in IP_addresses
-% 	gen_server:cast(?SERVER, {upload, File}).
+	% use loop to upload file to each IP_address in IP_addresses
+	gen_server:cast(?SERVER, {upload, File}).
+
+init_download(File) ->
+
+	erlang:display("CLIENT: initializing file download"),
+
+	% hash my fle
+
+	% tell monitor I want to download File
+	{RetMsg, IP_addresses} = gen_server:call(?SERVER, {init_download, File}),
+
+	erlang:display(RetMsg),
+
+	download(File, IP_addresses).
+
+
+download(File, IP_addresses) ->
+
+	erlang:display("CLIENT: now downloading file from peers"),
+
+	% use loop to request file from each IP_address in IP_addresses
+	gen_server:cast(?SERVER, {download, File}).
+
+init_remove(File) ->
+
+	erlang:display("CLIENT: initializing file removal"),
+
+	gen_server:cast(?SERVER, {init_remove, File}).
+
+
 
 
 % only for test. returns request count.
@@ -84,7 +127,17 @@ init([Port]) ->
 
 handle_cast({upload, File}, State) ->
 
-	erlang:display("uploading client file"),
+	erlang:display("SERVER: downloading client file"),
+	{noreply, State};
+
+handle_cast({download, File}, State) ->
+
+	erlang:display("SERVER: uploading client file"),
+	{noreply, State};
+
+handle_cast({init_remove, File}, State) ->
+
+	erlang:display("SERVER: received init_remove request"),
 	{noreply, State};
 
 handle_cast(stop, State) ->
@@ -100,27 +153,36 @@ handle_info({tcp, Socket, RawData}, State) ->
 % running the init/1 function
 handle_info(timeout, #state{lsock = ListenSocket} = State) ->
 
-	%use gen_tcp:accept/1 to wait for 
+	% use gen_tcp:accept/1 to wait for 
 	% a TCP connection on your listening socket
 	{ok, _Sock} = gen_tcp:accept(ListenSocket),
 
-	%returns and signals to the gen_server container that 
+	erlang:display("SERVER: client connected!"),
+
+	% returns and signals to the gen_server container that 
 	% you want to continue as normal with an unchanged state
 	{noreply, State}.
 
+% this is just for debugging
+% returns number of raw data requests
 handle_call(get_count, _From, State) ->
 
 	% reply indicates I want to send a reply to the client
-
 	% value returned to to client will be {ok, N}
 	% where N is the current number of requests
-
 	% set new server state to same as before
 	{reply, {ok, State#state.request_count}, State};
 
 handle_call({init_upload, File}, _From, State) ->
 
-	Reply = {"recieved init_upload request", [1,2,3,4]},
+	Reply = {"SERVER: recieved init_upload request", [1,2,3,4]},
+
+	% set new server state to same as before
+	{reply, Reply, State};
+
+handle_call({init_download, File}, _From, State) ->
+
+	Reply = {"SERVER: recieved init_download request", [1,2,3,4]},
 
 	% set new server state to same as before
 	{reply, Reply, State}.
